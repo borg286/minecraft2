67932fb91f37e20520ea06f2b87ca737cbe9f3f4
borg286 <borg286@gmail.com>
2018-03-08 20:53:09 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
 
+  $(document).ready(function(e) {
+  	$('section').tocible({ 
+    heading: 'h1', //[selector], the first level heading
+    subheading: 'h2', //[selector], the second level heading
+    reference:'.ref', title:'Contents'});
+  });
