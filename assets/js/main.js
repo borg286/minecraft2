142f46cb56447979441b9f2f5205715c3ce5eb63
borg286 <borg286@gmail.com>
2018-03-09 12:32:20 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,21 @@ function dynamicallyLoadsMyStuff() {
     var script = document.createElement("script");
     script.type = "javascript";
     script.src="https://ajax.cloudflare.com/cdn-cgi/scripts/935cb224/cloudflare-static/rocket.min.js"
-    document.footer.appendChild(script);
+    document.head.appendChild(script);
 
     var script2 = document.createElement("script");
     script2.type = "text/rocketscript";
     script2.setAttribute("data-rocketsrc","minecraft2/assets/js/jquery.tocible.js")
-    document.footer.appendChild(script2);
+    document.head.appendChild(script2);
 
     var script3 = document.createElement("script");
     script3.type = "text/rocketscript";
     script3.setAttribute("data-rocketsrc","minecraft2/assets/js/create.js")
-    document.footer.appendChild(script3);
+    document.head.appendChild(script3);
     
 }
 
 dynamicallyLoadsMyStuff();
+
+
 
