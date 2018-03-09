93adfc28862035072a1fb5364bf73cb48378c032
borg286 <borg286@gmail.com>
2018-03-09 12:44:20 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,11 @@ function dynamicallyLoadsMyStuff() {
     css.href = "/minecraft2/assets/css/jquery.tocible.css";
     var s = document.getElementsByTagName('link')[0]; s.parentNode.insertBefore(css, s);
 
-    var script = document.createElement("script");
-    script.type = "javascript";
-    script.async = true;
-    script.src="https://ajax.cloudflare.com/cdn-cgi/scripts/935cb224/cloudflare-static/rocket.min.js"
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);
+    var script3 = document.createElement("script");
+    script3.type = "text/rocketscript";
+    script3.async = true;
+    script3.setAttribute("data-rocketsrc","minecraft2/assets/js/create.js")
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script3, s);
 
     var script2 = document.createElement("script");
     script2.type = "text/rocketscript";
@@ -16,11 +16,11 @@ function dynamicallyLoadsMyStuff() {
     script2.setAttribute("data-rocketsrc","minecraft2/assets/js/jquery.tocible.js")
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script2, s);
 
-    var script3 = document.createElement("script");
-    script3.type = "text/rocketscript";
-    script3.async = true;
-    script3.setAttribute("data-rocketsrc","minecraft2/assets/js/create.js")
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script3, s);
+    var script = document.createElement("script");
+    script.type = "javascript";
+    script.async = true;
+    script.src="https://ajax.cloudflare.com/cdn-cgi/scripts/935cb224/cloudflare-static/rocket.min.js"
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);
     
 }
 
