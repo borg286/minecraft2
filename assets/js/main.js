c064fbceebf101971f143cb2c1914d57f7ec4921
borg286 <borg286@gmail.com>
2018-03-09 10:37:47 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,16 +12,16 @@ function dynamicallyLoadCss(url) {
     document.head.appendChild(script); // Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
 function addScripts() {
-  var div = document.createElement("div");
-  div.setAttribute("id", "ref");
-  document.getElementsByTagName('body')[0].appendChild(div);
   dynamicallyLoadScript("/minecraft2/assets/js/jquery.tocible.min.js");
   dynamicallyLoadCss("/minecraft2/assets/css/jquery.tocible.css");
 }
 addScripts();
-  $(document).ready(function(e) {
+$(document).ready(function(e) {
+  var div = document.createElement("div");
+  div.setAttribute("id", "ref");
+  document.getElementsByTagName('body')[0].appendChild(div);
   	$('section').tocible({ 
     heading: 'h1', //[selector], the first level heading
     subheading: 'h2', //[selector], the second level heading
     reference:'.ref', title:'Contents'});
-  });
+});
